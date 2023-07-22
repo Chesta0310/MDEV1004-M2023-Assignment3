7bfbf56a9c5c0bd46ea963fbb9a0607510e3f920
Chesta Patel <chestapatel123@gmail.com>
2023-07-22 01:17:40 -0400

    Added View

---
+++ MDEV1004-M2023-Assignment3-iOS/MDEV1004-M2023-Assignment3-iOS/MovieTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieTableViewCell.swift
+//  MDEV1004-M2023-Assignment3-iOS
+//
+//  Created by Chesta Patel on 22/07/23.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var studioLabel: UILabel!
+    
+    @IBOutlet weak var ratingLabel: UILabel!
+}
